9be63f3059163fd423dafbd969e9bbb20b134e93
Arun <vikrantkumarprajapati4@gmail.com>
2024-10-06 16:22:40 +0530

    initiated new todo add

--- 08-todo-app/src/main.jsx
+++ 08-todo-app/src/main.jsx
@@ -1,29 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Layout from "./Layout.jsx";
-import Home from "./components/Home.jsx";
-import Myday from "./components/Myday.jsx";
-import CompletedTasks from "./components/CompletedTasks.jsx";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="" element={<Home />} />
-      <Route path="myday" element={<Myday />} />
-      <Route path="completedtasks" element={<CompletedTasks />} />
-    </Route>
-  )
-);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
